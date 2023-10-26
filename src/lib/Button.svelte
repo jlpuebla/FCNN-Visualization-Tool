faeffccbe21ec6d7ab02c5d9c22f11e3aceed9bc
Elton V <43376123+eltonvilla@users.noreply.github.com>
2023-10-26 14:09:37 -0600

    Added create/sample nn buttons, need to work on toggles

---
+++ src/lib/Button.svelte
@@ -0,0 +1,23 @@
+<script>
+    let active = {clicked: false };
+    export let name = "default";
+
+    function onClick(){
+        active.clicked = !active.clicked;
+    }
+
+</script>
+
+<button on:click = {onClick}>
+    {name}
+</button>
+
+{#if active.clicked}
+    <br><br>
+    <label for="hlayers">Hidden Layers</label>
+    <input type="radio" id="hlayers" name="createnn"><br><br>
+    <label for="olayer">Output Layer</label>
+    <input type="radio" id="olayer" name="createnn"><br><br>
+    <label for="dataset">Dataset</label>
+    <input type="radio" id="dataset" name="createnn"><br><br>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import Button from '../lib/Button.svelte';
+</script>
+
+<br><br>
+<Button name = {"Create NN"}/>
+<br><br>
+<Button name = {"Sample NN"}/>
+
+//How to have only one button active at a time?
